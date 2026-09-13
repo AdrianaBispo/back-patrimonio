b2bcd95c997e5259ff72efd1b2a989c7a4b3ebd8
adrianaBispo283 <adriana.bispo283@gmail.com>
2026-09-13 19:16:32 -0300

    feat: criado api e adicionado auth

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthControllers\AuthController;
+use App\Http\Controllers\AuthControllers\LoginControllers;
+use App\Http\Controllers\AuthControllers\RegisterControllers;
+use App\Http\Controllers\AuthControllers\LogoutControllers;
+
+Route::prefix('auth')->group(function () {
+    Route::post('/login', [LoginControllers::class, 'login']);
+    Route::post('/register', [RegisterControllers::class, 'register']);
+    Route::post('/logout', [LogoutControllers::class, 'logout'])->middleware('jwt.auth');
+    Route::get('/user', [AuthController::class, 'user'])->middleware('jwt.auth');
+});
