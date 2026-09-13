0a258fff30a76fdd26de59a6ce8e7a757a893c68
adrianaBispo283 <adriana.bispo283@gmail.com>
2026-09-13 19:20:35 -0300

    refactor: ajustado JwtMiddleware

--- app/Middleware/JwtMiddleware.php
+++ app/Middleware/JwtMiddleware.php
@@ -3,24 +3,23 @@
 namespace App\Middleware;
 
 use Closure;
-use Tymon\JWTAuth\Exceptions\JWTException;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class JwtMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
         try {
-            $token = $request->header('Authorization');
-            if (! $token) {
-                return response()->json(['error' => 'Token not provided'], 401);
+            $user = JWTAuth::parseToken()->authenticate();
+            if (! $user) {
+                return response()->json(['error' => 'User not found'], 401);
             }
-            $token = str_replace('Bearer ', '', $token);
-            $request->merge(['token' => $token]);
-
-            return $next($request);
         } catch (JWTException $e) {
             return response()->json(['error' => 'Token is invalid'], 401);
         }
+
+        return $next($request);
     }
 }
