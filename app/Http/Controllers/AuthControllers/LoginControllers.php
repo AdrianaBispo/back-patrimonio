382d59342064b8b4b9dced9faaaf1d7be9cc6edb
adrianaBispo283 <adriana.bispo283@gmail.com>
2026-09-13 22:03:26 -0300

    refactor: adicionada novas validações no login

--- app/Http/Controllers/AuthControllers/LoginControllers.php
+++ app/Http/Controllers/AuthControllers/LoginControllers.php
@@ -1,36 +1,35 @@
 <?php
 
-    namespace App\Http\Controllers\AuthControllers;
-use Illuminate\Http\Request;
-use App\Models\User;
+namespace App\Http\Controllers\AuthControllers;
 use App\Http\Controllers\Controller;
-use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Http\Request;
 use Tymon\JWTAuth\Exceptions\JWTException;
-
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class LoginControllers extends Controller
 {
     public function login(Request $request)
     {
 
-    $data = $request->validate([
-        'email' => 'required|string|email|max:255',
-        'password' => 'required|string|min:8',
-    ], [
-        'email.required' => 'O email é obrigatório',
-        'email.email' => 'O email deve ser um email válido',
-        'email.max' => 'O email deve ter no máximo 255 caracteres',
-        'password.required' => 'A senha é obrigatória',
-        'password.min' => 'A senha deve ter no mínimo 8 caracteres',
-    ]);
-    try {
-        if (! $token = JWTAuth::attempt($data)) {
-            return response()->json(['error' => 'Credenciais inválidas'], 401);
+        $data = $request->validate([
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:8',
+        ], [
+            'email.required' => 'O email é obrigatório',
+            'email.email' => 'O email deve ser um email válido',
+            'email.max' => 'O email deve ter no máximo 255 caracteres',
+            'password.required' => 'A senha é obrigatória',
+            'password.min' => 'A senha deve ter no mínimo 8 caracteres',
+        ]);
+        try {
+            if (! $token = JWTAuth::attempt($data)) {
+                return response()->json(['error' => 'Credenciais inválidas'], 401);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'Erro ao gerar token'], 500);
         }
-    } catch (JWTException $e) {
-        return response()->json(['error' => 'Erro ao gerar token'], 500);
-    }
+        setcookie('access_token', $token, 60, '/');
+
         return response()->json(['access_token' => $token, 'token_type' => 'bearer']);
     }
-
 }
