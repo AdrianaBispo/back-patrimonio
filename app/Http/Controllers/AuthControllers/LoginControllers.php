b07e6a8dbec7e854cf319f32e3717c67be6fa509
adrianaBispo283 <adriana.bispo283@gmail.com>
2026-09-19 21:45:37 -0300

    refactor: ajustado o cookie

--- app/Http/Controllers/AuthControllers/LoginControllers.php
+++ app/Http/Controllers/AuthControllers/LoginControllers.php
@@ -10,17 +10,11 @@ class LoginControllers extends Controller
 {
     public function login(Request $request)
     {
-
         $data = $request->validate([
             'email' => 'required|string|email|max:255',
             'password' => 'required|string|min:8',
-        ], [
-            'email.required' => 'O email é obrigatório',
-            'email.email' => 'O email deve ser um email válido',
-            'email.max' => 'O email deve ter no máximo 255 caracteres',
-            'password.required' => 'A senha é obrigatória',
-            'password.min' => 'A senha deve ter no mínimo 8 caracteres',
         ]);
+      
         try {
             if (! $token = JWTAuth::attempt($data)) {
                 return response()->json(['error' => 'Credenciais inválidas'], 401);
@@ -28,8 +22,9 @@ class LoginControllers extends Controller
         } catch (JWTException $e) {
             return response()->json(['error' => 'Erro ao gerar token'], 500);
         }
-        setcookie('access_token', $token, 60, '/');
 
-        return response()->json(['access_token' => $token, 'token_type' => 'bearer']);
+        return response()
+            ->json(['access_token' => $token, 'token_type' => 'bearer'])
+            ->cookie('access_token', $token, 60, '/', null, false, true); 
     }
 }
