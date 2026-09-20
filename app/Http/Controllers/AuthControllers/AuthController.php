adad935db0fcca55157a7f524479f7c390987859
adrianaBispo283 <adriana.bispo283@gmail.com>
2026-09-19 21:46:15 -0300

    refactor: adicionado o cookie e colocado tratamentos

--- app/Http/Controllers/AuthControllers/AuthController.php
+++ app/Http/Controllers/AuthControllers/AuthController.php
@@ -1,15 +1,26 @@
 <?php
 
 namespace App\Http\Controllers\AuthControllers;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $user = $request->user();
         $token = $request->cookie('access_token');
-        return response()->json(compact('user', 'token'));
-    } 
+        
+        if (! $token) {
+            return response()->json(['message' => 'O cookie não chegou no backend'], 401);
+        } 
+        
+        try {
+            $user = JWTAuth::setToken($token)->authenticate();
+            return response()->json(['user' => $user]);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Token chegou, mas é inválido: ' . $e->getMessage()], 401);
+        }
+    }
 }
