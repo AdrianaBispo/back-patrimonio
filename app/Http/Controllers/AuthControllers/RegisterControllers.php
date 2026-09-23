89cc01905ac7a22d4e1e009485ee0a6483087eef
AdrianaBispo <adriana.bispo283@gmail.com>
2026-09-23 15:45:22 -0300

    refactor: adicionado a chamada do cookie com o token

--- app/Http/Controllers/AuthControllers/RegisterControllers.php
+++ app/Http/Controllers/AuthControllers/RegisterControllers.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use App\Http\Controllers\Controller;
 use Exception;
 use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
 
 class RegisterControllers extends Controller
 {
@@ -35,14 +37,23 @@ class RegisterControllers extends Controller
             'roles.array' => 'O perfil de acesso deve ser um array',
             'roles.exists' => 'O perfil de acesso informado não existe',
         ]);
+        $token = $request->cookie('access_token');
+        
+        if (! $token) {
+            return response()->json(['message' => 'O cookie não chegou no backend'], 401);
+        } 
 
         try {
-            $user = User::create($data, [
+            $user = JWTAuth::setToken($token)->authenticate();
+            if ($user->role !== 'admin') {
+                return response()->json(['message' => 'Você não tem permissão para registrar usuários'], 403);
+            }
+            $newUser = User::create($data, [
                 'password' => Hash::make($data['password']),
             ]);
-            return response()->json(['message' => 'Usuário registrado com sucesso'], 201);
+            return response()->json(['message' => 'Usuário registrado com sucesso', 'user' => $newUser], 201);
         } catch (Exception $e) {
-            return response()->json(['error' => 'Erro ao registrar usuário'], 500);
+            return response()->json(['error' => 'Erro ao registrar usuário: ' . $e->getMessage()], 500);
         }
     }
 }
