a622ea366b82fb5f9ef35a091575660a8ac6eff8
AdrianaBispo <adriana.bispo283@gmail.com>
2026-09-23 14:05:51 -0300

    feat: adicionado endpoint para obter todas as roles

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,15 @@ use App\Http\Controllers\AuthControllers\AuthController;
 use App\Http\Controllers\AuthControllers\LoginControllers;
 use App\Http\Controllers\AuthControllers\RegisterControllers;
 use App\Http\Controllers\AuthControllers\LogoutControllers;
+use App\Http\Controllers\RoleControllers\GetAllRolesControlles;
+
 
 Route::prefix('auth')->group(function () {
     Route::post('/login', [LoginControllers::class, 'login']);
-    Route::post('/register', [RegisterControllers::class, 'register']);
+    Route::post('/register', [RegisterControllers::class, 'register']);# colocar acesso apenas logado
     Route::post('/logout', [LogoutControllers::class, 'logout'])->middleware('jwt.auth');
     Route::get('/user', [AuthController::class, '__invoke']);
+});
+Route::prefix('roles')->group(function () {
+    Route::get('/all', [GetAllRolesControlles::class, '__invoke'])->middleware('role:Administrador');
 });
