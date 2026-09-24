5178e4080f368ba8776f351637980421984a84f8
AdrianaBispo <adriana.bispo283@gmail.com>
2026-09-24 17:17:00 -0300

    atualizado o import

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\AuthControllers\AuthController;
 use App\Http\Controllers\AuthControllers\LoginControllers;
 use App\Http\Controllers\AuthControllers\RegisterControllers;
 use App\Http\Controllers\AuthControllers\LogoutControllers;
-use App\Http\Controllers\AuthControllers\EditUserControllers;
+use App\Http\Controllers\UserControllers\EditUserController;
 use App\Http\Controllers\RoleControllers\GetAllRolesControlles;
 
 
@@ -13,8 +13,8 @@ Route::prefix('auth')->group(function () {
     Route::post('/register', [RegisterControllers::class, 'register']);
     Route::post('/logout', [LogoutControllers::class, 'logout'])->middleware('jwt.auth');
     Route::get('/user', [AuthController::class, '__invoke']);
-    Route::put('/users/{id}', [EditUserControllers::class, 'editUser']);
-});
+    Route::put('/users/{id}', [EditUserController::class, 'editUser']);
+}); 
 Route::prefix('roles')->group(function () {
     Route::get('/all', [GetAllRolesControlles::class, '__invoke'])->middleware('role:Administrador');
 });
