5231d4023e3a90c869ef3a7779c72f71c7b5d68c
AdrianaBispo <adriana.bispo283@gmail.com>
2026-09-24 14:44:56 -0300

    feat: adicionado EditUserControllers

--- routes/api.php
+++ routes/api.php
@@ -4,14 +4,16 @@ use App\Http\Controllers\AuthControllers\AuthController;
 use App\Http\Controllers\AuthControllers\LoginControllers;
 use App\Http\Controllers\AuthControllers\RegisterControllers;
 use App\Http\Controllers\AuthControllers\LogoutControllers;
+use App\Http\Controllers\AuthControllers\EditUserControllers;
 use App\Http\Controllers\RoleControllers\GetAllRolesControlles;
 
 
 Route::prefix('auth')->group(function () {
     Route::post('/login', [LoginControllers::class, 'login']);
-    Route::post('/register', [RegisterControllers::class, 'register']);# colocar acesso apenas logado
+    Route::post('/register', [RegisterControllers::class, 'register']);
     Route::post('/logout', [LogoutControllers::class, 'logout'])->middleware('jwt.auth');
     Route::get('/user', [AuthController::class, '__invoke']);
+    Route::put('/users/{id}', [EditUserControllers::class, 'editUser']);
 });
 Route::prefix('roles')->group(function () {
     Route::get('/all', [GetAllRolesControlles::class, '__invoke'])->middleware('role:Administrador');
